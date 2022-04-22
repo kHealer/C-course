b3c8bdf3d358fee0ae98c5f4c9b40f4eb78f7489
Safa HELVACI <95585412+kHealer@users.noreply.github.com>
2022-04-23 01:22:40 +0300

    Add files via upload

---
+++ else-if/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int time = DateTime.Now.Hour;
+if(time>=6 && time<11)
+    Console.WriteLine("Günaydın!");
+else if(time<= 18)
+    Console.WriteLine("iyi günler!");
+else
+    Console.WriteLine("İyi geceler!");
+string sonuc = time<=18 ? "iyi günler" : "iyi geceler";
+sonuc = time>=6 && time<11 ? "günaydın" : time<= 18 ? "İyi günler!" : "iyi geceler!";
+Console.WriteLine(sonuc);
